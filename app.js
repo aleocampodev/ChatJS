1be8881b7809c3a46d97f29c23d8b5e8f458dbc0
ocampoaleja <omaira.527@hotmail.com>
2020-05-21 19:27:07 -0500

    feature: added styles

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const button = document.querySelector('#button');
-const nameuser = document.querySelector('#user');
+const buttons = document.querySelector("#buttons");
+const nameUser = document.querySelector("#nameUser");
+const buttonOne = ` <button id="signIn">Cerrar sesion</button> `;
+const buttonTwo = ` <button id="access">Acceder</button> `
 
-firebase.auth().onAuthStateChanged(user => {
-    if(user)
+
+
+firebase.auth().onAuthStateChanged( user => {
+    if(user){
+        console.log('hi');
+        buttons.innerHTML= buttonTwo
+
+    }else{
+        console.log('no existe user');
+        buttons.innerHTML = buttonOne;
+
+    }
 })
