eca22b38ffe49e6b9b71f2841b9c96aff6e811bf
ocampoaleja <omaira.527@hotmail.com>
2020-05-21 21:20:03 -0500

    feature: modified navigation

--- app.js
+++ app.js
@@ -2,17 +2,58 @@ const buttons = document.querySelector("#buttons");
 const nameUser = document.querySelector("#nameUser");
 const buttonOne = ` <button id="signIn">Cerrar sesion</button> `;
 const buttonTwo = ` <button id="access">Acceder</button> `
+const form = document.querySelector('#form');
+const inputChange = querySelector('#inputChange')
 
 
 
 firebase.auth().onAuthStateChanged( user => {
     if(user){
-        console.log('hi');
-        buttons.innerHTML= buttonTwo
+        console.log('user');
+        buttons.innerHTML= buttonOne;
+        
+        nameUser.innerHTML= user.displayName;
+        signIn()
+        contentChat(user)
+
+        form.style.display='block'
 
     }else{
         console.log('no existe user');
-        buttons.innerHTML = buttonOne;
+        buttons.innerHTML = buttonTwo;
+
+        logIn()
+        nameUser.innerHTML='Chat';
+
+        form.style.display="none"
 
     }
-})
+});
+
+const logIn = () => {
+    const access = document.querySelector('#access');
+
+    access.addEventListener('click', async() => {
+        try{
+            const provider = new firebase.auth.GoogleAuthProvider();
+            await firebase.auth().signInWithPopup(provider)
+
+        } catch(error){
+            console.log(error)
+        }
+    })
+}
+
+const signIn = () => {
+    const signIn = document.querySelector('#signIn')
+
+    signIn.addEventListener('click' , () => {
+        firebase.auth().signOut();
+    })
+}
+
+const contentChat = (user) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+    })
+}
